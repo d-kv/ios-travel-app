ab854831fd8fbfc94e5f645f09336978221e556f
Ruslan Sorokin <strawberryladder@gmail.com>
2023-03-23 03:11:47 +0300

    style(Main): apply SwiftFormat

--- AfterWork/Main/MainViewPresenter.swift
+++ AfterWork/Main/MainViewPresenter.swift
@@ -6,87 +6,92 @@
 //
 
 import Foundation
-import UIKit
 import TinkoffID
+import UIKit
 
+// MARK: - MainViewPresenterDelegate
 
 protocol MainViewPresenterDelegate: AnyObject {
-    func mainViewPresenter(_ reposViewModel: MainViewPresenter,
-                            isLoading: Bool)
+  func mainViewPresenter(
+    _ reposViewModel: MainViewPresenter,
+    isLoading: Bool
+  )
 }
 
+// MARK: - MainViewPresenter
 
 final class MainViewPresenter {
-    weak var delegate: MainViewPresenterDelegate?
-    
-    func openSettings() {
-        let settingsViewController = DI.shared.getSettingsViewController()
-        
-        let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
-        sceneDelegate.window!.rootViewController?.present(settingsViewController, animated: true)
+  // MARK: Internal
+
+  weak var delegate: MainViewPresenterDelegate?
+
+  let preferences = UserDefaults.standard
+
+  func openSettings() {
+    let settingsViewController = DI.shared.getSettingsViewController()
+
+    let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
+    sceneDelegate.window!.rootViewController?.present(settingsViewController, animated: true)
+  }
+
+  func goToMap() {
+    let mapViewController = DI.shared.getMapViewController_Map()
+    mapViewController.modalPresentationStyle = .fullScreen
+
+    let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
+    sceneDelegate.window!.rootViewController?.present(mapViewController, animated: true)
+  }
+
+  func goToCards() {
+    let cardsViewController = DI.shared.getCardsViewController()
+    cardsViewController.modalPresentationStyle = .fullScreen
+
+    let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
+    sceneDelegate.window!.rootViewController?.present(cardsViewController, animated: true)
+  }
+
+  func goToLogin() {
+    let loginViewController = DI.shared.getLoginViewController()
+    loginViewController.modalPresentationStyle = .fullScreen
+
+    let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
+    sceneDelegate.window!.rootViewController?.present(loginViewController, animated: true)
+  }
+
+  func enteredApp() {
+    delegate?.mainViewPresenter(self, isLoading: true)
+
+    if !AuthService.tinkoffId.isTinkoffAuthAvailable {
+      delegate?.mainViewPresenter(self, isLoading: false)
+      DataLoader.loadData()
+    } else {
+      if preferences.string(forKey: "idToken") ?? nil != nil {
+        let refreshToken = preferences.string(forKey: "refreshToken") ?? ""
+
+        AuthService.tinkoffId.obtainTokenPayload(using: refreshToken, handleRefreshToken)
+      } else {
+        delegate?.mainViewPresenter(self, isLoading: false)
+        goToLogin()
+      }
     }
-    
-    func goToMap() {
-        let mapViewController = DI.shared.getMapViewController_Map()
-        mapViewController.modalPresentationStyle = .fullScreen
-        
-        let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
-        sceneDelegate.window!.rootViewController?.present(mapViewController, animated: true)
+  }
+
+  // MARK: Private
+
+  private func handleRefreshToken(_ result: Result<TinkoffTokenPayload, TinkoffAuthError>) {
+    do {
+      let credentials = try result.get()
+
+      preferences.set(credentials.idToken, forKey: "idToken")
+      preferences.set(credentials.accessToken, forKey: "accessToken")
+      preferences.set(credentials.refreshToken, forKey: "refreshToken")
+
+      DataLoader.loadData()
+
+      delegate?.mainViewPresenter(self, isLoading: false)
+    } catch {
+      delegate?.mainViewPresenter(self, isLoading: false)
+      goToLogin()
     }
-    
-    func goToCards() {
-        let cardsViewController = DI.shared.getCardsViewController()
-        cardsViewController.modalPresentationStyle = .fullScreen
-        
-        let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
-        sceneDelegate.window!.rootViewController?.present(cardsViewController, animated: true)
-    }
-    
-    func goToLogin() {
-        let loginViewController = DI.shared.getLoginViewController()
-        loginViewController.modalPresentationStyle = .fullScreen
-        
-        let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
-        sceneDelegate.window!.rootViewController?.present(loginViewController, animated: true)
-        
-    }
-    
-    let preferences = UserDefaults.standard
-    
-    func enteredApp() {
-        
-        self.delegate?.mainViewPresenter(self, isLoading: true)
-        
-        if !AuthService.tinkoffId.isTinkoffAuthAvailable {
-            self.delegate?.mainViewPresenter(self, isLoading: false)
-            DataLoader.loadData()
-        } else {
-            if preferences.string(forKey: "idToken") ?? nil != nil {
-                
-                let refreshToken = preferences.string(forKey: "refreshToken") ?? ""
-                
-                AuthService.tinkoffId.obtainTokenPayload(using: refreshToken, handleRefreshToken)
-            } else {
-                self.delegate?.mainViewPresenter(self, isLoading: false)
-                goToLogin()
-            }
-        }
-    }
-    
-    private func handleRefreshToken(_ result: Result<TinkoffTokenPayload, TinkoffAuthError>) {
-        do {
-            let credentials = try result.get()
-            
-            preferences.set(credentials.idToken, forKey: "idToken")
-            preferences.set(credentials.accessToken, forKey: "accessToken")
-            preferences.set(credentials.refreshToken, forKey: "refreshToken")
-                        
-            DataLoader.loadData()
-            
-            self.delegate?.mainViewPresenter(self, isLoading: false)
-        } catch {
-            self.delegate?.mainViewPresenter(self, isLoading: false)
-            goToLogin()
-        }
-    }
+  }
 }
